3f15f27be5162e53974e80056f3d528ea97dcdc3
Coty Beasley <hi@coty.email>
2019-07-05 17:19:05 -0700

    Added all files

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Public Variables
+    public float runSpeed;
+
+    // Private Variables
+    private float moveInput;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
